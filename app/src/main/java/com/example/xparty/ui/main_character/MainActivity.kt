488696519a125c3cd90b309cdb0a0192711356e4
Daniel George <daniel96geo@gmail.com>
2022-12-09 14:23:31 +0200

    Main fragment style change and add navigation drawer support

--- app/src/main/java/com/example/xparty/ui/main_character/MainActivity.kt
+++ app/src/main/java/com/example/xparty/ui/main_character/MainActivity.kt
@@ -2,11 +2,71 @@ package com.example.xparty.ui.main_character
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.xparty.R
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    private lateinit var navigationView: NavigationView
+    private var isOpen: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Call findViewById on the DrawerLayout
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        // Pass the ActionBarToggle action into the drawerListener
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, 0, 0)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+
+        // Display the hamburger icon to launch the drawer
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Call syncState() on the action bar so it'll automatically change to the back button when the drawer layout is open
+        actionBarDrawerToggle.syncState()
+
+
+        // Call findViewById on the NavigationView
+        navigationView = findViewById(R.id.navView)
+
+        // Call setNavigationItemSelectedListener on the NavigationView to detect when items are clicked
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.mainFragmentStart_menu -> {
+                    Toast.makeText(this, "My Profile", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_second_fragment -> {
+                    Toast.makeText(this, "People", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_third_fragment -> {
+                    Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        if (isOpen) {
+            drawerLayout.closeDrawer(navigationView)
+            isOpen = false
+            return false
+        } else {
+            drawerLayout.openDrawer(navigationView)
+            isOpen = true
+            return true
+        }
     }
 }
